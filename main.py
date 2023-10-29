fab482a75393138092267ee42053791cf4211f71
Maksim L <maksim.lopatin.spb@gmail.com>
2023-10-30 00:44:15 +0300

    Чтение файла

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello')
+import xmltodict
+
+with open('smartphone.xml', encoding='utf-8') as xml_file:
+  data = xmltodict.parse(xml_file.read())
+
+decision = data['decision']
+
+problem = decision['problem']
+criterionList = decision['criterionList']
+importance = decision['importance']
+scale = decision['scale']
+variantList = decision['variantList']
